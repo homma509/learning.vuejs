07e1543329e92e78f91df5749f4541b8c5e46767
homma509 <kouki.honma@gmail.com>
2020-08-02 13:36:23 +0900

    add: lec18

---
+++ lec18/script.js
@@ -0,0 +1,18 @@
+const vm = new Vue({
+  el: '#app',
+  template: `
+    <div v-if="message">
+      {{ message }}
+    </div>
+    <div v-else>
+      メッセージがありません
+    </div>
+  `,
+  data() {
+    return {
+      message: 'こんにちは'
+    }
+  }
+})
+
+window.vm = vm

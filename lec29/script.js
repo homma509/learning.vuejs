6f071e7905aef85d4e75bf046e1d80a4924f08c5
homma509 <kouki.honma@gmail.com>
2020-08-10 08:39:05 +0900

    add: lec29

---
+++ lec29/script.js
@@ -0,0 +1,21 @@
+const Layout = {
+  template: `
+    <div class="container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  `
+}
+const vm = new Vue({
+  el: '#app',
+  components: {
+    'layout': Layout
+  }
+})
